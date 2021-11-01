bb62912b00ab1a620c8379985374235f9607966c
cmankos <35984116+cmankos@users.noreply.github.com>
2021-10-31 22:36:08 -0400

    Create deck.py

---
+++ deck.py
@@ -0,0 +1,7 @@
+"""
+
+Broad strokes:
+
+Like the idea of app Leo has where you put in experience and it suggests cards. Want that more fleshed out for positions and customizable, with set packs (1/2guard pack, for instance) and user created
+
+"""
